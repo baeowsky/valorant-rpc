1931b16a9862a9be26ab575a02b861f6bdec21cc
bae <85345064+baeowsky@users.noreply.github.com>
2025-12-19 01:44:48 +0100

    fix(queue): Fix matchmaking queue status detection

--- src/presence/presences/menu.py
+++ src/presence/presences/menu.py
@@ -6,6 +6,9 @@ def presence(rpc,client=None,data=None,content_data=None,config=None):
         "MATCHMAKING": queue,
         "CUSTOM_GAME_SETUP": custom_setup,
     }
-    party_state = data.get('partyState', 'DEFAULT') if data else 'DEFAULT'
+    party_data = data.get('partyPresenceData', {}) if data else {}
+    match_data = data.get('matchPresenceData', {}) if data else {}
+    party_state = party_data.get('partyState') or match_data.get('partyState') or data.get('partyState', 'DEFAULT') if data else 'DEFAULT'
+    
     if party_state in state_types.keys():
         state_types[party_state].presence(rpc,client=client,data=data,content_data=content_data,config=config)
